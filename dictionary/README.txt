3f30c1182440f7eb25810e0e041cf3e49143be53
Thiago <thiagozoid@gmail.com>
2020-04-14 23:35:46 -0300

    Mover dicionário

---
+++ dictionary/README.txt
@@ -0,0 +1,4 @@
+George A. Miller (1995). WordNet: A Lexical Database for English.
+Communications of the ACM Vol. 38, No. 11: 39-41.
+
+Christiane Fellbaum (1998, ed.) WordNet: An Electronic Lexical Database. Cambridge, MA: MIT Press.
--- squad.c
+++ squad.c
@@ -1,11 +1,3 @@
-/**
- * Dictionary by:
- * George A. Miller (1995). WordNet: A Lexical Database for English.
- * Communications of the ACM Vol. 38, No. 11: 39-41.
-
- * Christiane Fellbaum (1998, ed.) WordNet: An Electronic Lexical Database. Cambridge, MA: MIT Press.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
